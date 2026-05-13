9047b18aaa62391729f906e9d18763ca4f39da89
data-spice <myvicgit@gmail.com>
2026-05-13 16:11:17 +0300

    Pr2: Practiced on all concepts from lesson 1

---
+++ practice2/data_set.py
@@ -0,0 +1,47 @@
+students: list[dict]=[
+    {
+      "id": 1,
+      "name": "Amina Hassan",
+      "age": 20,
+      "course": "Computer Science",
+      "year": "2.1",
+      "email": "amina.hassan@example.com",
+      "gpa": 3.6
+    },
+    {
+      "id": 2,
+      "name": "Brian Otieno",
+      "age": 22,
+      "course": "Information Technology",
+      "year": "3.2",
+      "email": "brian.otieno@example.com",
+      "gpa": 3.2
+    },
+     {
+      "id": 3,
+      "name": "Caroline Wanjiku",
+      "age": 19,
+      "course": "Software Engineering",
+      "year": "1.2",
+      "email": "caroline.wanjiku@example.com",
+      "gpa": 3.8
+    },
+    {
+      "id": 4,
+      "name": "David Mwangi",
+      "age": 21,
+      "course": "Data Science",
+      "year": "2.2",
+      "email": "david.mwangi@example.com",
+      "gpa": 3.4
+    },
+    {
+      "id": 5,
+      "name": "Esther Njeri",
+      "age": 23,
+      "course": "Cyber Security",
+      "year": "4.1",
+      "email": "esther.njeri@example.com",
+      "gpa": 3.9
+    }
+]
