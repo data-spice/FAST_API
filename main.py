4423f804f4199b71c42695eaa91387383bc61686
data-spice <myvicgit@gmail.com>
2026-05-13 07:56:53 +0300

    L2: Learnt Jinja2 , Template inheritance and StaticFiles

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.templating import Jinja2Templates
+from lesson_1 import posts
+app=FastAPI()
+templates=Jinja2Templates(directory="templates")
 
-app= FastAPI()
-
-@app.get('/')
-async def root ():
-    return{"Hello":"World"}
-
-@app.get("/about")
-async def about() -> list:
-    return 'Some exceptional company'
-
+@app.get("/")
+async def root(request:Request):
+    return templates.TemplateResponse(
+    "home.html",
+    {
+        "request": request,
+        "posts": posts,
+        "title": "Home"
+    }
+)
