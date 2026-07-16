68ea949e994cef8f0e5f1d287d1844f3a979d786
data-spice <myvicgit@gmail.com>
2026-07-16 23:46:37 +0300

    DSA quick sort

--- data_tools/random_codez.py
+++ data_tools/random_codez.py
@@ -1,11 +1,23 @@
-def count_even(arr):
-    counter=0
-    for i in range(0, len(arr)):
-        if arr[i]%2==0:
-            counter+=1
-    return counter
+def partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+
+    for j in range (low,high):
+        if arr[j] <= pivot:
+            i+=1
+            arr[i], arr[j]= arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
 
 
-nums = [1, 2, 4, 7, 10, 13, 18]
+def quicksort(arr,low,high):
+    if low < high:
+        piv=partition(arr,low,high)
+        quicksort(arr,low,piv-1)
+        quicksort(arr,piv+1,high)
 
-print(count_even(nums))
+nums = [5, 2, 9, 1, 9, 7]
+
+quicksort(nums, 0, len(nums) - 1)
+
+print(nums)
